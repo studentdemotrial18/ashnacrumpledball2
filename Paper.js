b3b3bb0353af1b7d675fce104183e74ecdad1607
studentdemotrial18 <73699927+studentdemotrial18@users.noreply.github.com>
2021-01-10 07:14:47 +0400

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -7,9 +7,9 @@ class Paper
             friction:0.5,
             density:1.2
         }
-        this.body=Bodies.circle(100,550,20,options);
-        this.width=20;
-        this.height=20;
+        this.body=Bodies.circle(100,550,40,options);
+        this.r=40;
+      
         this.image=loadImage("paper.png");
 
         World.add(world,this.body);
@@ -19,7 +19,7 @@ class Paper
     display()
     { 
         imageMode (CENTER);
-        image (this.image,100,550,this.width,this.height);
+        image (this.image,100,550,this.r);
         
         
     }
