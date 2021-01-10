317a1720923a66c96ac814d4b659f1968244d11a
studentdemotrial18 <73699927+studentdemotrial18@users.noreply.github.com>
2021-01-10 18:01:16 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -4,17 +4,22 @@ class Box
         var options={
             isStatic:true
         }
-        this.body=Bodies.rectangle(x,y,width,height,options);
         this.width=width;
         this.height=height;
-
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        
+        this.image=loadImage("dustbingreen.png");
         World.add(world,this.body);
     }
     display()
     {
-        rectMode(CENTER);
-        fill ("white");
-        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+        var pos=this.body.position;
+        push();
+       translate(pos.x, pos.y);
+       rotate (this.angle);
+        imageMode (CENTER);
+        image (this.image,0,-70,this.width,this.height);
+        pop ();
     
     }
 }
