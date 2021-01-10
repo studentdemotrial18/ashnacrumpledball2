5687159eb1a1f6b31fabf8d41c98d7090921c045
studentdemotrial18 <73699927+studentdemotrial18@users.noreply.github.com>
2021-01-10 18:13:48 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,17 +1,17 @@
 
 class Paper
 {
-    constructor(x,y){
+    constructor(x,y,radius){
         var options={
             
             restitution:0.3,
             friction:2.5,
             density:1.2
         }
-        this.radius=70;
+        this.radius=radius;
         this.x=x;
         this.y=y;
-        this.body=Bodies.circle(this.x,this.y,30,options);
+        this.body=Bodies.circle(this.x,this.y,this.radius,options);
         this.image=loadImage("paper.png");
         World.add(world,this.body);
 
@@ -21,7 +21,7 @@ class Paper
     { 
        var pos=this.body.position;
        imageMode (CENTER);
-       image (this.image,pos.x,pos.y,this.radius,this.radius);
+       image (this.image,pos.x,pos.y,this.radius*2,this.radius*2);
        
     }
 }
